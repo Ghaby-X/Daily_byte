9bcb18064ab95cedadbc25db8017b0f9e4d96a04
Ghaby-X <gabrielanyaele21@gmail.com>
2024-03-18 13:37:36 +0000

    optimized from O(nlogn) to O(n)

--- strings/valid_anagram.py
+++ strings/valid_anagram.py
@@ -7,11 +7,37 @@
 # s = "listen", t = "silent", return true
 # s = "program", t = "function", return false
 
-def is_valid_anagram(s : str, t: str):
-    s_sort = ''.join(sorted([*s]))
-    t_sort = ''.join(sorted([*t]))
 
-    print(sorted(s) == sorted(t))
+# def is_valid_anagram(s : str, t: str):
+#     s_sort = ''.join(sorted([*s]))
+#     t_sort = ''.join(sorted([*t]))
+
+#     print(sorted(s) == sorted(t))
+
+
+def is_valid_anagram(s:str, t: str):
+    map = dict()
+    
+    for i in s:
+        if i in map:
+            map[i] =map[i] + 1
+        else:
+            map[i] = 1
+    
+    for i in t:
+        if i in map:
+            map[i] -= 1
+        else:
+            print(False)
+            return
+    
+    for i in map.keys():
+        if map[i] != 0:
+            print(False)
+            return
+    
+    print(True)
+
 
 #tests
 is_valid_anagram('cat', 'tac')
