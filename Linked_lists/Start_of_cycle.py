b374835901adae2289e5167cb66ad78d448fd309
Ghaby-X <gabrielanyaele21@gmail.com>
2024-04-02 11:09:33 +0000

    add floyd cycle algorithm

--- Linked_lists/Start_of_cycle.py
+++ Linked_lists/Start_of_cycle.py
@@ -10,7 +10,7 @@ from Linked_List import create_linked_list, Node
 
 def start_of_cycle(lst):
     cycle_start_reference = None
-    current = lst.head
+    current = lst
     
     dictionary = set()
     
@@ -27,6 +27,61 @@ def start_of_cycle(lst):
     print('None')
     return
 
-start_of_cycle(create_linked_list([1,2,3])) # -->> return null
-start_of_cycle(create_linked_list([1,2,3,4,5,2])) # -->> return reference to the node containing 2
-start_of_cycle(create_linked_list([1,9,3,7,7])) # -->> return reference to node containing 7
+def floyd_cycle_algorithm(lst):
+    
+    slow = lst
+    fast = lst
+    
+    while(fast != None and fast.next != None):
+        slow = slow.next
+        fast = fast.next.next
+        
+        if slow == fast:
+            break
+    
+    if slow != fast:
+        print('None')
+        return
+    
+    slow = lst
+    
+    while(slow, fast and fast.next):
+        if slow == fast:
+            cycle_start = slow
+            print(cycle_start.val)
+            return
+        
+        slow = slow.next
+        fast = fast.next
+        
+    pass
+
+
+#testss
+lst1 = Node(1)
+lst1.next = Node(2)
+lst1.next.next = Node(3)
+
+lst2 = Node(1)
+lst2.next = Node(2)
+lst2.next.next = Node(3)
+lst2.next.next.next = Node(4)
+lst2.next.next.next.next = Node(5)
+lst2.next.next.next.next.next = lst2.next
+
+lst3 = Node(1)
+lst3.next = Node(9)
+lst3.next.next = Node(3)
+lst3.next.next.next = Node(7)
+lst3.next.next.next.next = lst3.next.next.next
+
+
+start_of_cycle(lst1) # -->> return null
+start_of_cycle(lst2) # -->> return reference to the node containing 2
+start_of_cycle(lst3)# -->> return reference to node containing 73)
+
+
+
+floyd_cycle_algorithm(lst1) # -->> return null
+floyd_cycle_algorithm(lst2) # -->> return reference to the node containing 2
+floyd_cycle_algorithm(lst3) # -->> return reference to node containing 7
